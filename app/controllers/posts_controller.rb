080b18523df91315364b0c7ac1633ddb27f9597d
Simon Bailey <simonbailey.uk@ntlworld.com>
2015-02-04 20:57:05 +0000

    Set up categorys and posts databases

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    sdfs  = 0
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
