1ccd127a98a105f5199f4757857e31514909ee0a
Simon Bailey <simonbailey.uk@ntlworld.com>
2015-02-10 22:37:30 +0000

    Added categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,3 @@
 class Category < ActiveRecord::Base
-
   has_many :posts
-
-  private
-
-  def location_params
-    params.require(:location).permit(:name)
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,4 @@
 class Post < ActiveRecord::Base
 
-  belongs_to :cagegory
-
-  private
-
-  def location_params
-    params.require(:location).permit(:title, :body, :category_id, :author_id)
-  end
+  belongs_to :category
 end
