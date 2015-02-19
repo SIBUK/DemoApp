3a3b6cef79329478271491d803baa72ab564c371
SimonMobica <simon.bailey@mobica.com>
2015-02-19 15:55:17 +0000

    Fixed javascripts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( jquery-1.7.2.min.js )
+Rails.application.config.assets.precompile += %w( ddsmoothmenu.js )
+Rails.application.config.assets.precompile += %w( retina.js )
+Rails.application.config.assets.precompile += %w( selectnav.js )
+Rails.application.config.assets.precompile += %w( jquery.masonry.min.js )
+Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
+Rails.application.config.assets.precompile += %w( jquery.backstretch.min.js )
+Rails.application.config.assets.precompile += %w( mediaelement.min.js )
+Rails.application.config.assets.precompile += %w( mediaelementplayer.min.js )
+Rails.application.config.assets.precompile += %w( jquery.dcflickr.1.0.js )
+Rails.application.config.assets.precompile += %w( twitter.min.js )
+Rails.application.config.assets.precompile += %w( scripts.js )
