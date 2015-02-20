22b871d7787c72c0c2e1663fd01ab1e35315e6e4
SimonMobica <simon.bailey@mobica.com>
2015-02-20 15:12:41 +0000

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
-          bucket: ENV['S3_BUCKET_NAME'],
+          bucket: 'elasticbeanstalk-us-east-1-457423554937',
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_KEY']
       }
