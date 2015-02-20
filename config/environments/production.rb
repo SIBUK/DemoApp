e627b2d6d4cb8d6e313da419d86b35ee8f41d213
SimonMobica <simon.bailey@mobica.com>
2015-02-20 13:00:34 +0000

    Changed bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-          :bucket => 'elasticbeanstalk-us-east-1-457423554937',
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_KEY']
+      storage: :s3,
+      s3_credentials: {
+          bucket: ENV['S3_BUCKET_NAME'],
+          access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_KEY']
       }
   }
 end
