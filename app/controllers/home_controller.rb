e5a45c5cabcade4eacc48cfb69e57635e61a0ab5
SimonMobica <simon.bailey@mobica.com>
2015-03-10 17:27:21 +0000

    Implemented Post Archive function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = params[:sel] ? Post.find(params[:sel]) : Post.all
     @popular = Post.order('num_likes DESC').limit(3)
   end
 end
