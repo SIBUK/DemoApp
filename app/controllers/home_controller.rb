3eeed031d06e92633ed087007c0365b036d7d3d1
SimonMobica <simon.bailey@mobica.com>
2015-03-16 18:44:33 +0000

    Added basic functionality to browse through older/newer posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
+  @@display_count = 2
+
   def index
+    if params[:page]
+      @page = params[:page].to_i
+      offset = [0, @page * @@display_count].max
+    else
+      @page = 0
+    end
+
     @q = Post.search(params[:q])
-    @posts = params[:sel] ? Post.find(params[:sel]) : @q.result(distinct: true)
+    @posts = params[:sel] ? Post.find(params[:sel]) : @q.result(distinct: true).limit(@@display_count).offset(offset)
     @popular = Post.order('num_likes DESC').limit(3)
   end
 end
