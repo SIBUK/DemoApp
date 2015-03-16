747b8ebb39163dfbcccc828c6e04aa0cd1a6587c
SimonMobica <simon.bailey@mobica.com>
2015-03-16 14:30:34 +0000

    Enhancements to some of the views. Partials added

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,6 @@
 class Pic < ActiveRecord::Base
   belongs_to :post
 
-  has_attached_file :image, :styles => { large: "1920x1080", medium: "1280x720", small: "640x360", thumb: "96x96#" }#, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { large: "1920x1080>", medium: "1280x720>", small: "640x360>", thumb: "128x128#" }#, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
