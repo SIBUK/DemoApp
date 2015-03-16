5c13b74c0983845631ecef4f7b3b4ea07ff1d8c1
Simon Bailey <simonbailey.uk@ntlworld.com>
2015-03-16 23:23:51 +0000

    Finished older/newer posts navigator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  @@display_count = 2
+  @@display_count = 10
 
   def index
     if params[:page]
@@ -7,10 +7,12 @@ class HomeController < ApplicationController
       offset = [0, @page * @@display_count].max
     else
       @page = 0
+      offset = 0
     end
 
     @q = Post.search(params[:q])
     @posts = params[:sel] ? Post.find(params[:sel]) : @q.result(distinct: true).limit(@@display_count).offset(offset)
+    @more = (offset + @@display_count) < @q.result(distinct: true).count
     @popular = Post.order('num_likes DESC').limit(3)
   end
 end
