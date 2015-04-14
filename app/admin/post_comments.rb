60eac367df55b90c51775b7393790420a93cb521
SimonMobica <simon.bailey@mobica.com>
2015-04-14 12:13:13 +0100

    ActiveAdmin ifx (had to permit params for comments)

--- app/admin/post_comments.rb
+++ app/admin/post_comments.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register PostComment do
   menu priority: 4
+
+  permit_params :all
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
